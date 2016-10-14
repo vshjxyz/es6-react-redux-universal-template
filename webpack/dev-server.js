ad9826ae09f5ed80f60a064c810915174ddb4b17
Luca Del Bianco <vshjxyz@gmail.com>
2016-10-14 16:35:09 +0200

    Updating dependencies :arrows_counterclockwise:

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -4,10 +4,12 @@ require('babel-register');
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
+var DashboardPlugin = require('webpack-dashboard/plugin');
 
 var config = require('./dev.config');
 
 var compiler = webpack(config.webpack);
+compiler.apply(new DashboardPlugin());
 var devServer = new WebpackDevServer(compiler, config.server.options);
 
 devServer.listen(config.server.port, 'localhost', function () {
